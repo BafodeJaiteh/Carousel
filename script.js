aa4065806a429f1f8eac597b478ed4ac1693f0fa
bafode <bafodejaiteh2@gmail.com>
2019-09-27 12:46:43 +0200

    add loups

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-'use strict'
+"use strict";
+
+const button = document.querySelector(".button");
+
+const li= document.querySelectorAll("li");
+
+let position = 0;
+
+function nextImage() {
+    position++;
+    if (position == li.length) {
+        position= 0;
+    }
+
+    for(let i=0 ; i< li.length;i++){
+    li[i].classList.remove("active");
+}
+li[position].classList.add("active");
+
+}
+
+button.addEventListener ("click",nextImage);
+
